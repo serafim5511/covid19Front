a9c40af195a7fe5a986f14ab9782c2ac30b82ff5
Gabriel Serafim Souza <gabrielserafimsouza@hotmail.com>
2021-09-18 15:45:09 -0300

    clean house

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+
 module.exports = {
-  reactStrictMode: true,
-  i18n: {
-    locales: ['pt-BR','en-US'],
-    defaultLocale: 'pt-BR',
-      }
-}
+  exportPathMap: async function() {
+    const paths = {
+      '/': { page: '/' }
+    };
+    return paths; //<--this was missing previously
+  }
+};
