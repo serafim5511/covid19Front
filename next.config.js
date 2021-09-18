8674dde303023dc7a1f5049d5b4604d4beacbd3b
Gabriel Serafim Souza <gabrielserafimsouza@hotmail.com>
2021-09-18 15:46:45 -0300

    teste ajuste

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-
 module.exports = {
-  exportPathMap: async function() {
-    const paths = {
-      '/': { page: '/' }
-    };
-    return paths; //<--this was missing previously
-  }
-};
+  reactStrictMode: true,
+  i18n: {
+    locales: ['pt-BR'],
+    defaultLocale: 'pt-BR',
+      }
+}
